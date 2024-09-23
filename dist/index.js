0bb0f206000ad0738f357beefa3f35ccd3c8938d
gecitemre <egecit@trustlab.com>
2024-09-23 13:39:12 +0300

    Add logging for PR number retrieval in getPullRequestNumber function

--- dist/index.js
+++ dist/index.js
@@ -62,6 +62,7 @@ function getPullRequestNumber(branch, owner, repo) {
             repo,
             head: `${owner}:${branch}`,
         });
+        console.log("PR number:", response.data[0].number);
         return response.data[0].number;
     });
 }
